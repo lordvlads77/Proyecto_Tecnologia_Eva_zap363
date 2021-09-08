87fe1b5b4f45b17859658348d64538423361600e
lordvlads77 <81182279+lordvlads77@users.noreply.github.com>
2021-09-08 00:59:53 -0500

    Proyecto HelloWorld

--- include/header.hh
+++ include/header.hh
@@ -1,4 +1,15 @@
+#include <string>
 int Add(int a, int b)
+{
+    return a + b;
+}
+
+std::string Add(std::string a, std::string b)
+{
+    return a +b;
+}
+
+float Add(float a, float b)
 {
     return a + b;
 }
--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,98 @@
 
 /*No usar "using namespce std;"*/
 
+
+
 int main()
 {
-    std::cout<< "Suma: " << Add(1, 2) ;
+    /*
+    int numberInteger{1}; //Así se inicializa apropiadamente algo en C++
+
+    int numberIntegerr{}; //Significa que el valor es 0 de inicio.*/
+
+    //float numberFloat{5.f }; /*Tambien puedes inicializar floats*/
+
+    //const char* stringChar{"My string char"}; /*Inicializando un charizard*/
+
+    //char chararray[]{"char array"};
+
+    //std::string myString{"My string from STL "};
+    
+    /*int *numPointerArr
+    {
+        new int[3]
+        {
+            1, 2, 3
+        }
+    };
+
+    std::cout << *numPointerArr << std::endl;
+
+    std::cout << numberInteger << std::endl; 
+    std::cout << numberFloat << std::endl;
+    std::cout << stringChar << "Size: " << sizeof(stringChar) << std::endl;
+    std::cout << chararray << "Size: " << sizeof(chararray) << std::endl;
+    std::cout << myString << "Size: " << sizeof(myString) << std::endl;
+   */
+
+   /*for(int i{}; i < 5; i++)
+   {
+       std::cout << stringChar + 1 << std::endl;
+   }*/
+
+   /*for(int i{}; i < 3; i++)
+   {
+       std::cout << *(numPointerArr + i) << std::endl;
+   }*/
+
+    /*std::cout<< "Suma: " << Add(1.0f, 2.5f) ;*/
+    
+    int number{5};
+    int number2{number};
+    int& numberRef{number};
+    int* numberPointer{&number};
+    int** numberPointer2{&numberPointer};
+
+    std::cout << "number variable: " << number << std::endl;
+    std::cout << "number2 variable: " << number2 << std::endl;
+    std::cout << "number reference: " << numberRef << std::endl;
+    std::cout << std::endl;
+    numberRef++;
+    std::cout << "number variable: " << number << std::endl;
+    std::cout << "number2 variable: " << number2 << std::endl;
+    std::cout << "number reference: " << numberRef << std::endl;
+    std::cout << std::endl;
+    std::cout << "number variable: " << &number << std::endl;
+    std::cout << "number2 variable: " << &number2 << std::endl;
+    std::cout << "number reference: " << &numberRef << std::endl;
+    /*numberRef = number2;
+    std::cout << std::endl;
+    std::cout << "number variable: " << number << std::endl;
+    std::cout << "number2 variable: " << number2 << std::endl;
+    std::cout << "number reference: " << numberRef << std::endl;
+    std::cout << std::endl;
+    std::cout << "number variable: " << &number << std::endl;
+    std::cout << "number2 variable: " << &number2 << std::endl;
+    std::cout << "number reference: " << &numberRef << std::endl;*/
+
+
+    std::cout << std::endl;
+    std::cout << "number Pointer memory: " << numberPointer << std::endl;
+    std::cout << "number pointer value: " << numberPointer << std::endl;
+    std::cout << "number pointer reference: " << &numberPointer << std::endl;
+    numberPointer = &number2;
+    std::cout << std::endl;
+    std::cout << "number Pointer memory: " << numberPointer << std::endl;
+    std::cout << "number pointer value: " << numberPointer << std::endl;
+    std::cout << "number pointer reference: " << &numberPointer << std::endl;
+    std::cout << std::endl;
+    std::cout << "pointer to pointer: " << **numberPointer2;
+
+
+        
+
+
     std::cin.get();
     return 0;
 }
+
